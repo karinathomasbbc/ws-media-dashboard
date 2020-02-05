f427f4b6b14d5a32008fe2f14ada3f33f0b4b10e
Karina Thomas <karina.thomas@bbc.co.uk>
2020-02-05 11:21:24 +0000

    Bengali - Home page - Live = Simorgh

--- index.js
+++ index.js
@@ -548,7 +548,7 @@ const services = [
           },
           {
             env: "live",
-            renderer: "PAL",
+            renderer: "Simorgh",
             path: ""
           }
         ]
