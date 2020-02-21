8024ee33c83159f8de62ee5de4834ea982b5488b
Karina Thomas <karina.thomas@bbc.co.uk>
2020-02-21 17:36:48 +0000

    Korean - PGL - Test/Stage/Live=Simorgh

--- index.js
+++ index.js
@@ -2660,17 +2660,17 @@ const services = [
         environments: [
           {
             env: "test",
-            renderer: "PAL",
+            renderer: "Simorgh",
             path: ""
           },
           {
             env: "stage",
-            renderer: "PAL",
+            renderer: "Simorgh",
             path: ""
           },
           {
             env: "live",
-            renderer: "PAL",
+            renderer: "Simorgh",
             path: ""
           }
         ]
