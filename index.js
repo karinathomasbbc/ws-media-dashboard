ce8acc93ab84c2dc3f42811305fa337e155cc316
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-26 21:56:06 +0000

    Arabic - Home page - Test=Simorgh

--- index.js
+++ index.js
@@ -614,7 +614,7 @@ const services = [
         environments: [
           {
             env: "test",
-            renderer: "PAL",
+            renderer: "Simorgh",
             path: ""
           },
           {
